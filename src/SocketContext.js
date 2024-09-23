09a5b8e774c405bf4d030e75db80ce7ff0af909c
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-09-23 16:04:13 +0530

    SocketContext using Hooks

---
+++ src/SocketContext.js
@@ -0,0 +1,32 @@
+import React, { createConte, useState, useRef, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const SocketContext = createContext();
+
+const socket = io('http://localhost:5000');
+
+const ContextProvider = ({ children }) => {
+  const [stream, setStream] = useState(null);
+  const [me, setMe] = useState('');
+  const myVideo = useRef(); 
+  useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true})
+        .then((currentStream) => {
+            setStream(currentStream);
+            myVideo.current.srcObject = currentStream;
+        });
+        socket.io('me', (id) => setMe(id));
+    });
+    const answerCall = () => {
+
+  } 
+  
+  const callUser = () => {
+
+  }
+
+  const leaveCall = () => {
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Import ThemeProvider
 import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createTheme(); // Create a default theme
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
